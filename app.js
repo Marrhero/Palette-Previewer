95f2b13d896fe932bcc474064707530ff0ce33e5
Marrhero <Marrsadiq@gmail.com>
2023-09-13 18:43:08 +0100

    Added color selection options
    User can now select colors through a color selection field if they prefer.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const color_window = document.querySelector("color_window");
 const hexInput =  document.getElementById("hex_input");
 const hexInput2 =  document.getElementById("hex_input2");
+const rgbInput =  document.getElementById("rgb_input");
+const rgbInput2 =  document.getElementById("rgb_input2");
+
+
 const changeColorBtn =  document.getElementById("change_color_button");
 const changeColorBtn2 =  document.getElementById("change_color_button2");
 
@@ -15,12 +19,44 @@ const bodyButton = document.getElementById("body_button");
 //Button for inverting colors
 const invertButton = document.getElementById("invert_button");
 
+const rgbRadio = document.getElementById("rgb");
+const hexRadio = document.getElementById("hex");
+
+document.getElementById("hex_div").style.display = "none";
+document.getElementById("rgb_div").style.display = "block";
+
+var isHex = false;
+
+rgbRadio.addEventListener("click", ()=>{
+
+  console.log("rgb radio registered");
+  document.getElementById("hex_div").style.display = "none";
+  document.getElementById("rgb_div").style.display = "block";
+})
+
+hexRadio.addEventListener("click", ()=>{
+  console.log("hex radio registered");
+  document.getElementById("rgb_div").style.display = "none";
+  document.getElementById("hex_div").style.display = "block";
+})
+
 changeColorBtn.addEventListener("click", ()=>{
+  if(isHex)
+  {
     const hexCode = "#" + hexInput.value;
     document.documentElement.style.setProperty("--text-color", hexCode);
 
     const hexCode2 = "#" + hexInput2.value;
     document.documentElement.style.setProperty("--bg-color", hexCode2);
+  }
+  else
+  {
+    const rgbCode = rgbInput.value;
+    document.documentElement.style.setProperty("--text-color", rgbCode);
+
+    const rgbCode2 = rgbInput2.value;
+    document.documentElement.style.setProperty("--bg-color", rgbCode2);
+  }
 });
 
 titleButton.addEventListener("click", ()=>{
